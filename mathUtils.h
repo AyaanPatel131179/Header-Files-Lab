1da67ba1a15838e5c75aa829e5a5d03edeed0f9e
Ayaan Patel <199788428+AyaanPatel131179@users.noreply.github.com>
2026-04-29 08:28:33 -0400

    Create mathUtils.h with basic math function declarations
    Added function declarations for basic math operations.

---
+++ mathUtils.h
@@ -0,0 +1,13 @@
+#ifndef MATHUTILS_H //gaurds against multiple inclusions of the header file
+#define MATHUTILS_H
+
+//fucntions declarations for basic math operations
+int add(int a, int b);
+
+int subtract(int a, int b);
+
+int multiply(int a, int b);
+
+double divide(double a, double b);
+
+#endif
